edb9fc83b368280095a2d137fd99a0f37bdac6ac
Lucciana <lucciana.caceres19@gmail.com>
2026-07-23 10:58:28 -0500

    Create Phaser game setup without npm

---
+++ js/config.js
@@ -0,0 +1,17 @@
+const config = {
+
+    type: Phaser.AUTO,
+
+    width: 320,
+    height: 180,
+
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+
+    backgroundColor: "#000000",
+
+    scene: []
+
+};
